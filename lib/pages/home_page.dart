73fb086562e7d615b0b24a8f0316459ba18db0ab
niyaz <niyaz1998@yandex.ru>
2021-05-05 19:03:07 +0300

    removed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -35,43 +35,36 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: GestureDetector(
-              onTapDown: (TapDownDetails details) => _damage(details),
-              child: Stack(
-                children: [
-                  Container(
-                    alignment: Alignment(0, -0.6),
-                    child: Text(
-                      '$_bossDamage',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w800,
-                        color: Colors.deepPurple,
-                        fontSize: 20,
-                      ),
-                    ),
-                    color: Colors.blue,
-                  ),
-                  Center(
-                    child: Image.asset("assets/gameimage/player/boss.png"),
-                  ),
-                  AnimatedContainer(
-                    duration: Duration(milliseconds: 0),
-                    alignment: Alignment(0, 1),
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Image.asset('$_pers'),
-                    ),
-                  ),
-                ],
+      body: GestureDetector(
+        onTapDown: (TapDownDetails details) => _damage(details),
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment(0, -0.6),
+              child: Text(
+                '$_bossDamage',
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.deepPurple,
+                  fontSize: 20,
+                ),
+              ),
+              color: Colors.blue,
+            ),
+            Center(
+              child: Image.asset("assets/gameimage/player/boss.png"),
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 0),
+              alignment: Alignment(0, 1),
+              child: Container(
+                height: 100,
+                width: 100,
+                child: Image.asset('$_pers'),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
